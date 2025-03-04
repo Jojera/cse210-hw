92fc3e376ab6ca182e63d11ad22b011e4cba0799
Jojera <jojera@byui.edu>
2025-03-04 09:19:49 +0300

    Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // core requirement 1
+        // Console.Write("What is your magic number? ");
+        // string userInput = Console.ReadLine();
+        // int magicNumber = int.Parse(userInput);
+
+        // core requirement 3
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        
+        // core requiremnent 2
+        int guess = 0;
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+            
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }    
     }
 }
