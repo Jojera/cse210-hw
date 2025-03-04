c44b4b97e67e078d7510e78bb2b0e5b12c532e16
Jojera <jojera@byui.edu>
2025-03-04 13:47:25 +0300

    Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -3,7 +3,47 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    {   
+        DisplayWelcome();
+
+        string name = PromptUserName();
+
+        int favNumber = PromptUserNumber();
+
+        int square = SquareNumber(favNumber);
+
+        DisplayResult(name, square);
+    }    
+    static void DisplayWelcome()
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine("Welcome to the prorgram.");
+    }   
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string fullName = Console.ReadLine();
+        return fullName;
+    } 
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine()); 
+        return number;
     }
+
+    static int SquareNumber(int number)
+    {
+        int sqNumber = number * number;
+        return sqNumber; 
+    }
+
+    static void DisplayResult(string fullName, int sqNumber)
+    {
+        Console.WriteLine($"{fullName}, the square of your number is {sqNumber}");
+    }
+
+
+    
 }
