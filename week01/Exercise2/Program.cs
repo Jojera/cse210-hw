82e45677acf0e6adfd3dcd3221df417858a2fc96
Jojera <jojera@byui.edu>
2025-03-04 08:17:26 +0300

    Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -3,7 +3,47 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+    {   
+        // core requirement 1 and 3
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+        
+
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter ="A";
+        }
+        else if (percentage >= 80)
+        {
+            letter ="B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Grade: {letter}");
+
+        // core requirement 2
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed.");
+        }
+        else
+        {
+            Console.WriteLine("Please work harder next time.");
+        }
+
     }
 }
